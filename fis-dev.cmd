1236ac0bc239c89b7a4ee971b37de0d2a94d5173
Julien.zhang <julien.zhang@chinascopefinancial>
2015-03-12 16:30:51 +0800

    optimize

--- fis-dev.cmd
+++ fis-dev.cmd
@@ -1,1 +1,2 @@
-fis server start --type node --port 3000 --root ./express & fis release --file fis-conf/dev.js --dest local -D  --clean --watch
+:: fis server start --type node --port 3000 --root ./express &
+fis release --file fis-conf/dev.js --dest local -D  --clean --watch
